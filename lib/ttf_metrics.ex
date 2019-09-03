055af090e0d3c36cd6c7bc54fa00688dacef1d29
Herman verschooten <Herman@verschooten.net>
2019-09-03 18:02:42 +0200

    Add ascend, descend and line_gap

--- lib/ttf_metrics.ex
+++ lib/ttf_metrics.ex
@@ -7,17 +7,9 @@ defmodule TTFMetrics do
 
   alias TTFMetrics.Directory
   alias TTFMetrics.Table
-  defstruct [:ascent]
+  defstruct [:ascent, :descent, :line_gap]
 
-  @doc """
-  parse/1
-
-  ## Examples
-
-      iex> TTFMetrics.parse("test/fonts/DejaVuSans.ttf")
-      {:ok, %TTFMetrics{}}
-
-  """
+  @spec parse(String.t()) :: {:ok, TTFMetrics.t()}
   def parse(font_file) do
     contents = File.read!(font_file)
     directory = Directory.parse(contents)
@@ -32,7 +24,12 @@ defmodule TTFMetrics do
 
     IO.inspect(tables)
 
-    {:ok, %TTFMetrics{}}
+    {:ok,
+     %TTFMetrics{
+       ascent: ascent(tables),
+       descent: descent(tables),
+       line_gap: line_gap(tables)
+     }}
   end
 
   defp parse_table("OS/2", data) do
@@ -44,4 +41,34 @@ defmodule TTFMetrics do
   end
 
   defp parse_table(_, _), do: %{}
+
+  defp ascent(%{"OS/2" => os2, "hhea" => hhea}) do
+    if os2.ascent != 0 do
+      os2.ascent
+    else
+      hhea.ascent
+    end
+  end
+
+  defp ascent(_), do: nil
+
+  defp descent(%{"OS/2" => os2, "hhea" => hhea}) do
+    if os2.descent != 0 do
+      os2.descent
+    else
+      hhea.descent
+    end
+  end
+
+  defp descent(_), do: nil
+
+  defp line_gap(%{"OS/2" => os2, "hhea" => hhea}) do
+    if os2.line_gap != 0 do
+      os2.line_gap
+    else
+      hhea.line_gap
+    end
+  end
+
+  defp line_gap(_), do: nil
 end
--- test/ttf_metrics_test.exs
+++ test/ttf_metrics_test.exs
@@ -2,7 +2,29 @@ defmodule TTFMetricsTest do
   use ExUnit.Case
   doctest TTFMetrics
 
-  test "returns a %TTFMetrics{}" do
-    assert {:ok, %TTFMetrics{}} == TTFMetrics.parse("test/fonts/DejaVuSans.ttf")
+  setup do
+    {:ok, font1} = TTFMetrics.parse("test/fonts/DejaVuSans.ttf")
+    {:ok, font2} = TTFMetrics.parse("test/fonts/ColorTestSbix.ttf")
+
+    {:ok, font1: font1, font2: font2}
+  end
+
+  test "returns a %TTFMetrics{}", %{font1: font} do
+    assert %TTFMetrics{} = font
+  end
+
+  test "has an ascent", %{font1: font1, font2: font2} do
+    assert font1.ascent == 1556
+    assert font2.ascent == 220
+  end
+
+  test "has a descent", %{font1: font1, font2: font2} do
+    assert font1.descent == -492
+    assert font2.descent == -80
+  end
+
+  test "has a line_gap", %{font1: font1, font2: font2} do
+    assert font1.line_gap == 410
+    assert font2.line_gap == 0
   end
 end
